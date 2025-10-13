8bf4859d86de6275a1776be8adb8acd4bb448e88
Patrick Abde <patrickabde@mac.home>
2025-10-13 12:26:23 +0200

    Added event listeners and function for reading input.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 "use strict";
 
 // Eventlyssnare för DOM-inladdning och knapptryck.
-
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#generate").addEventListener("click", addInfo);
+    document.querySelector("#clear").addEventListener("click");
+});
 
 
 // Funktion för att läsa in all inmatad data.
+function addInfo() {
 
+    // läs in namn
+    const nameEl = document.querySelector("#fullname").value;
+
+    // läs in mail
+    const emailEl = document.querySelector("#email").value;
+
+    // läs in telefonnummer
+    const phoneEl = document.querySelector("#phone").value;
 
 
 // Kontrollera den inmatade informationen.
@@ -18,6 +30,8 @@
 
 // Skriv ut all information till DOM.
 
+
+}
 
 
 // Funktion för att rensa alla fält.
