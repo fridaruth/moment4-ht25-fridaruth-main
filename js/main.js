0035c780308450c92377173e0d1a8dfb89e5aacd
Patrick Abde <patrickabde@mac.home>
2025-10-13 12:05:03 +0200

    First commit. Added outlines of functions needed.

---
+++ js/main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// Eventlyssnare för DOM-inladdning och knapptryck.
+
+
+
+// Funktion för att läsa in all inmatad data.
+
+
+
+// Kontrollera den inmatade informationen.
+
+
+
+// Skapa felmeddelanden.
+
+
+
+// Skriv ut all information till DOM.
+
+
+
+// Funktion för att rensa alla fält.
+
